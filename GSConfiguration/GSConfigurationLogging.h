53c64f852ae8cd3c2493ec921e6277326f1be9e3
Ryan Brignoni <castral01@gmail.com>
2015-02-21 01:40:48 -0800

    Removing CocoaLumberjack support for now.

--- GSConfiguration/GSConfigurationLogging.h
+++ GSConfiguration/GSConfigurationLogging.h
@@ -6,25 +6,13 @@
 //  Copyright (c) 2015 Ryan Brignoni. All rights reserved.
 //
 
-#if !defined(GS_CONFIG_LOGGING_ENABLED) && __has_include("CocoaLumberjack.h")
-    #define GS_CONFIG_LOGGING_ENABLED 1
-#endif
-
 #if GS_CONFIG_LOGGING_ENABLED
 
-    #if __has_include("CocoaLumberjack.h")
-        #import <CocoaLumberjack/CocoaLumberjack.h>
-        #define GSLogDebug(fmt, ...) DDLogDebug(fmt, ##__VA_ARGS__)
-        #define GSLogInfo(fmt, ...)  DDLogInfo(fmt, ##__VA_ARGS__)
-        #define GSLogWarn(fmt, ...)  DDLogWarn(fmt, ##__VA_ARGS__)
-        #define GSLogError(fmt, ...) DDLogError(fmt, ##__VA_ARGS__)
-    #else
-        #define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
-        #define GSLogDebug(fmt, ...) DLog(fmt, ##__VA_ARGS__)
-        #define GSLogInfo(fmt, ...)  DLog(fmt, ##__VA_ARGS__)
-        #define GSLogWarn(fmt, ...)  DLog(fmt, ##__VA_ARGS__)
-        #define GSLogError(fmt, ...) DLog(fmt, ##__VA_ARGS__)
-    #endif
+    #define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+    #define GSLogDebug(fmt, ...) DLog(fmt, ##__VA_ARGS__)
+    #define GSLogInfo(fmt, ...)  DLog(fmt, ##__VA_ARGS__)
+    #define GSLogWarn(fmt, ...)  DLog(fmt, ##__VA_ARGS__)
+    #define GSLogError(fmt, ...) DLog(fmt, ##__VA_ARGS__)
 
 #else
 
